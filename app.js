15f3ff536fc389d1815d20f233e2feed8489d5d5
Quyen Lu <quyen.lu@fastboy.net>
2022-02-18 01:37:21 -0500

    Route Handling

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+// Can omit the .js at the end of the file since
+// it will get added automatically
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 const PORT = process.env.PORT || 3000;
 
+// Register a middleware 
+// This will automatically call next() 
+// This will do the whole request parsing (request body)
+// But not for file
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
 app.use((req, res, next) => {
-    console.log('Hello World');
-})
+    res.status(404).send('<h1>Page not found</h1>');
+
+});
+
+
+
 
 app.listen(PORT);
 
---
+++ routes/shop.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const router = express.Router();
+
+app.get('/', (req, res, next) => {
+    res.send('<h1>Hello from Express</h1>');
+});
+
+
+module.exports = routers;
